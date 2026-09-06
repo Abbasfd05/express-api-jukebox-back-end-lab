3413d04f32931bcaf3d3987e5fbfcc66f1188680
Abbasfd05 <abbasfadhel2005abfd@gmail.com>
2026-09-06 16:17:45 +0300

    LAB

--- controllers/TrackCtrl.js
+++ controllers/TrackCtrl.js
@@ -1,1 +1,63 @@
-const mongoose=require('mongoose');
+const Track = require('../models/Track.js');
+
+// POST /tracks
+async function create(req, res) {
+  try {
+    const track = await Track.create(req.body);
+    res.status(201).json(track);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+// GET /tracks
+async function index(req, res) {
+  try {
+    const tracks = await Track.find({});
+    res.status(200).json(tracks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+// GET /tracks/:id
+async function show(req, res) {
+  try {
+    const track = await Track.findById(req.params.id);
+    res.status(200).json(track);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+// PUT /tracks/:id
+async function update(req, res) {
+  try {
+    const updatedTrack = await Track.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedTrack);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+// DELETE /tracks/:id
+async function deleteTrack(req, res) {
+  try {
+    const deletedTrack = await Track.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedTrack);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+module.exports = {
+  create,
+  index,
+  show,
+  update,
+  delete: deleteTrack,
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors');
+
+// Controllers
+const TrackCtrl = require('./controllers/TrackCtrl');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// Routes go here
+app.post('/tracks', TrackCtrl.create);
+app.get('/tracks', TrackCtrl.index);
+app.get('/tracks:id', TrackCtrl.show);
+app.put('/tracks:id', TrackCtrl.update);
+app.delete('/tracks:id', TrackCtrl.delete);
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
